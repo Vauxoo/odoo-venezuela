4f5eb0f2f5f81f95a8f53d8cf45dcaedf646ac21
javieredm@openerpvzla <>
2011-03-31 15:20:41 -0430

    [IMP] se validan que los campos de RIF y codigo del partner sean unicos, en el modulo base_partner_invoice

--- base_partner_invoice/partner.py
+++ base_partner_invoice/partner.py
@@ -45,11 +45,37 @@ class res_partner(osv.osv):
 
         return False
 
+    def _check_partner_code(self,cr,uid,ids,context={}):
+        partner_brw = self.browse(cr,uid,ids[0])
+        if partner_brw.ref:
+            part_ids = self.search(cr, uid, [('ref','=',partner_brw.ref)], limit=2, context=context)
+            if part_ids:
+                res = dict(map(lambda x:(x,True),part_ids))
+                res.pop(partner_brw.id,False)
+                if res:
+                    return False
+
+        return True
+
+    def _check_partner_vat(self,cr,uid,ids,context={}):
+        partner_brw = self.browse(cr,uid,ids[0])
+        if partner_brw.vat:
+            part_ids = self.search(cr, uid, [('vat','=',partner_brw.vat)], limit=2, context=context)
+            if part_ids:
+                res = dict(map(lambda x:(x,True),part_ids))
+                res.pop(partner_brw.id,False)
+                if res:
+                    return False
+
+        return True
+
 
     _constraints = [
-        (_check_partner_invoice_addr, 'Error ! No ha definido una direccion fiscal. ', ['address'])
+        (_check_partner_invoice_addr, 'Error ! No ha definido una direccion fiscal. ', ['address']),
+        (_check_partner_code, 'El codigo del partner. ya fue asignado, revise por favor !. ', []),
+        (_check_partner_vat, 'El R.I.F. ya fue asignado, revise por favor !. ', []),        
     ]
-
+    
 
 res_partner()
 
