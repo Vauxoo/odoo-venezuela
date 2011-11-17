749fa4ceafdda5125d382af87988e02bea9dd335
Gabriela (Vauxoo) <gabrielaquilarque97@gmail.com>
2011-11-17 16:55:18 -0430

    [IMP] The sql added for group xml lines.

--- l10n_ve_islr_withholding/islr_xml_wh.py
+++ l10n_ve_islr_withholding/islr_xml_wh.py
@@ -129,18 +129,26 @@ class islr_xml_wh_doc(osv.osv):
             
             period = wh_brw.period_id.name.split('/')
             period2 = period[1]+period[0]
-
+            
+            sql= '''SELECT partner_vat,control_number,porcent_rete,concept_code,invoice_number, SUM(COALESCE(base,0)) as base  
+            FROM islr_xml_wh_line 
+            WHERE period_id= %s 
+            GROUP BY partner_vat,control_number,porcent_rete,concept_code,invoice_number'''%(wh_brw.period_id.id)
+            cr.execute(sql)
+            xml_lines=cr.fetchall()
+            
             root = Element("RelacionRetencionesISLR")
             root.attrib['RifAgente'] = wh_brw.company_id.partner_id.vat[2:]
             root.attrib['Periodo'] = period2.strip()
-            for line in wh_brw.xml_ids:
+            for line in xml_lines:
+                partner_vat,control_number,porcent_rete,concept_code,invoice_number,base=line
                 detalle = SubElement(root,"DetalleRetencion")
-                SubElement(detalle, "RifRetenido").text = line.partner_vat
-                SubElement(detalle, "NumeroFactura").text = line.invoice_number
-                SubElement(detalle, "NumeroControl").text = line.control_number
-                SubElement(detalle, "CodigoConcepto").text = line.concept_code
-                SubElement(detalle, "MontoOperacion").text = str(line.base)
-                SubElement(detalle, "PorcentajeRetencion").text = str(line.porcent_rete)
+                SubElement(detalle, "RifRetenido").text = partner_vat
+                SubElement(detalle, "NumeroFactura").text = invoice_number
+                SubElement(detalle, "NumeroControl").text = control_number
+                SubElement(detalle, "CodigoConcepto").text = concept_code
+                SubElement(detalle, "MontoOperacion").text = str(base)
+                SubElement(detalle, "PorcentajeRetencion").text = str(porcent_rete)
         #~ ElementTree(root).write("/home/gabriela/openerp/Gabriela/5.0/Helados Gilda/islr_withholding/xml.xml")
         self.indent(root)
         return tostring(root,encoding="ISO-8859-1")
